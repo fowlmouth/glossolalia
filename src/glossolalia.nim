bc6aaa58d9cff1327410266b4bd6a775c9b4d6f8
fowlmouth <phowl.mouth@gmail.com>
2014-05-19 19:18:53 -0500

    added v4

---
+++ src/glossolalia.nim
@@ -0,0 +1,370 @@
+##
+## γλωσσολαλία
+## 
+## Revision 4
+##   Optimization: capture position and length instead of allocating
+##     new strings. Now, allocations only happen on a save() or at
+##     the end, if no nodes are consumed. 
+##   & and | changed to `and` and `or`, the precedence works better
+##
+## Revision 3
+##   Rules are generic, they match to a generic AST node.
+##   Use `save(rule) do (match: string|seq[N]) -> N: MyNode(...)` 
+##     to save a capture
+##
+## Revision 2
+##   Rules match to build a JSON tree. This ends up not working well.
+##
+## Revision 1
+##   Rules return strings.
+##   Operators implemented:
+##     combination &, |, *, +, ? 
+##   Basic matchers
+##     str(), chr() 
+##
+import 
+  strutils,
+  fowltek/maybe_t
+
+type
+  TInput* = object
+    str: string
+    len,pos: int
+  
+  TMatchKind* = enum mUnrefined, mString, mNodes
+  TMatch*[N] = object
+    case kind*: TMatchKind
+    of mUnrefined:
+      pos, len: int
+    of mString:
+      str: string
+    of mNodes:
+      nodes: seq[N]
+
+  TMatchResult* [N] = TMaybe[TMatch[N]]
+  Rule* [N] = ref object
+    m: proc(input:var TInput): TMatchResult[N]
+
+template matchf (body:stmt):expr{.immediate.}=
+  (proc(input: var TInput): TMatchResult[N] =
+    body)
+
+proc currentChar* (I:TInput):char = I.str[I.pos]
+
+template chrMatcher (chars): expr =
+  (matchf do:
+    if input.currentChar in chars:
+      result = just(TMatch[N](
+        kind: mUnrefined, 
+        pos: input.pos, 
+        len: 1
+      ))
+      input.pos.inc)
+
+proc charMatcher* [N] (chrs: set[char]): Rule[N] =
+  Rule[N](m: matchf do:
+    if input.currentChar in chrs:
+      result = just(TMatch[N](
+        kind: mUnrefined, 
+        pos: input.pos, 
+        len: 1
+      ))
+      input.pos.inc)
+
+proc charMatcher* [N] (chrs: varargs[char]): Rule[N] =
+  let chrs = @chrs
+  Rule[N](
+    m: matchf do:
+      if input.currentChar in chrs:
+        result = just(TMatch[N](
+          kind: mUnrefined, 
+          pos: input.pos, 
+          len: 1
+        ))
+        input.pos.inc
+  )
+
+proc strMatcher* [N] (str: string): Rule[N] =
+  # Matches a string, case sensitive
+  Rule[N](
+    m: matchf do:
+      if input.str.continuesWith(str, input.pos):
+        result = just(tmatch[n](
+          kind: mUnrefined,
+          pos: input.pos,
+          len: str.len
+        ))
+        input.pos.inc str.len 
+  )
+
+proc accumulate [N] (matches: varargs[TMatch[N]]): TMatch[N] =
+  # saves positive matches by joining arrays of 
+  # saved AST nodes or concatenating raw strings 
+  assert matches.len > 0
+  
+  #try to find saved nodes
+  var found_nodes = false
+  for it in matches:
+    if it.kind == mNodes:
+      if result.kind != mNodes:
+        result = TMatch[N](kind: mNodes, nodes: it.nodes)
+        found_nodes = true
+      else:
+        result.nodes.add it.nodes
+  if found_nodes: return
+
+  #all strings, add up the captures
+  result = TMatch[N](kind: mUnrefined, pos: matches[0].pos)
+  var high = result.pos + matches[0].len
+  for i in 1 .. <len(matches):
+    high = max(matches[i].pos + matches[i].len, high)
+  result.len = high - result.pos
+
+
+proc `and`* [N] (a,b: Rule[N]): Rule[N] =
+  Rule[N](
+    m: matchf do:
+      let start = input.pos
+      if (let (has,m1) = a.m(input); has):
+        if (let (has,m2) = b.m(input); has):
+          result = just(accumulate(m1, m2))
+          return
+      input.pos = start
+  )
+
+proc `or`* [N] (a,b: Rule[N]): Rule[N] =
+  Rule[N](
+    m: matchf do:
+      let start = input.pos
+      if (let (has,m) = a.m(input); has):
+        return just(m)
+      input.pos = start
+      if (let (has,m) = b.m(input); has):
+        return just(m)
+      input.pos = start
+  )
+
+proc present* [N] (R:Rule[N]): Rule[N] =
+  Rule[N](
+    m: matchf do:
+      let start = input.pos
+      if r.m(input):
+        result = just(Tmatch[N](kind: mUnrefined, pos: start, len: 0))
+      input.pos = start
+  )
+proc absent* [N] (R:Rule[N]): Rule[N] =
+  Rule[N](
+    m: matchf do:
+      let start = input.pos
+      if not r.m(input):
+        result = just(TMatch[N](kind: mUnrefined, pos:start, len:0))
+      input.pos = start
+  )
+
+
+
+proc good_match [n] (input:TInput; len:int): TMatchResult[N] =
+  just(TMatch[N](kind: mUnrefined, pos: input.pos, len: len))
+proc good_match [n] (nodes: varargs[N]): TMatchResult[N] =
+  just(TMatch[N](kind: mNodes, nodes: @nodes))
+
+proc repeat* [N] (R:Rule[N]; min,max:int): Rule[N] =
+  Rule[N](
+    m: matchf do:
+      var matches = 0
+      let startPos = input.pos
+      var results: seq[TMatch[N]] = @[]
+      
+      while input.pos < input.len and matches < max:
+        if (let(has,res) = r.m(input); has):
+          results.add res
+          inc matches, 1
+          continue
+        break
+
+      if matches < min:
+        input.pos = startPos
+        #result = match_fail
+      else:
+        if matches > 0:
+          result = just(accumulate(results))
+        else:
+          result = good_match[n](input,0)
+  )
+
+
+proc repeat* [N] (R:Rule[N]; min:int): Rule[N] =
+  Rule[N](
+    m: matchf do:
+      var matches = 0
+      let startPos = input.pos
+      var results: seq[TMatch[N]] = @[]
+      
+      while input.pos < input.len:
+        if (let (has, res) = r.m(input); has):
+          results.add res
+          inc matches
+          continue
+        break
+      
+      if matches < min:
+        input.pos = startPos
+        #result = match_fail
+      else:
+        if matches > 0:
+          result = just(accumulate(results))
+        else:
+          result = good_match[n](input,0)
+  )
+proc `+`* [N] (R:Rule[N]): Rule[N] = R.repeat(1)
+proc `*`* [N] (R:Rule[N]): Rule[N] = R.repeat(0)
+proc `?`* [N] (R:Rule[N]): Rule[N] = R.repeat(0,1)
+
+proc high [N] (match: TMatch[N]): int = match.pos + match.len - 1
+
+proc save* [N] (R:Rule[N]; cb: proc(match:string): N): Rule[N] =
+  # store a string as an `N`
+  # use it to catch butterflies!
+  Rule[N](
+    m: matchf do:
+      result = r.m(input)
+      if result.has and result.val.kind == mUnrefined:
+        result = good_match[n](
+          cb(input.str.substr(result.val.pos, result.val.high))
+        )
+  )
+proc save* [N] (R:Rule[N]; cb: proc(match: seq[N]): N): Rule[N] =
+  # save a sequence of nodes as a node
+  Rule[N](
+    m: matchf do:
+      result = r.m(input)
+      if result.has and result.val.kind == mNodes:
+        result = good_match[n](cb(result.val.nodes))
+  )
+
+proc match* [N] (rule:Rule[N]; str:string): TMatchResult[N] =
+  var input = TInput(str: str, pos: 0, len: str.len)
+  result = rule.m(input)
+  if result.has and result.val.kind == mUnrefined:
+    echo result
+    let high = result.val.len+result.val.pos-1
+    result = just(TMatch[N](
+      kind: mString,
+      str: input.str.substr(result.val.pos, high)
+    ))
+
+
+
+
+
+import macros
+
+
+proc `:=`* [N] (a, b: Rule[N]) =
+  # update rule a, set its matcher to rule b
+  # you can use this to refer to rules before 
+  # they're initialized.
+  a.m       = b.m
+  discard """ a.tos     = b.tos
+  a.tos_alt = b.tos_alt """
+proc newRule* [N] (): Rule [N] =
+  # returns an uninitialized rule. you should give semantics
+  # with `myrule := chr('G','T',...)`
+  Rule[N]()
+
+macro genGrammar(TNode:expr; body:stmt):stmt {.immediate.}=
+  # accepts a list of statements like
+  #
+  #   hello := "Hello"
+  #   digits <- many(chr( {'0' .. '9'} ))
+  #
+  # you can refer to a rule here before it is defined
+  #
+  assert body.kind == nnkStmtList
+  result = newStmtList()
+  let varDecl = newNimNode(nnkVarSection)
+  result.add varDecl
+  
+  for i in 0 .. < len(body):
+    let s = body[i]
+    if s.kind == nnkInfix and $(s[0]) in [":=","<-"]:
+      varDecl.add newIdentDefs(
+        s[1], 
+        newEmptyNode(), 
+        newNimNode(nnkCall).add(
+          newNimNode(nnkBracketExpr).add(ident"newRule", TNode)))
+      result.add  s[1].infix(":=", s[2])
+    else:
+      result.add s
+
+  when defined(Debug):
+    echo repr(result)
+
+
+#proc save (R:Rule[TNode]; cb:proc(match:string):TNode): Rule[TNode] = saveMatcher[TNode](cb)
+#template save (a,b): expr = saveMatcher[TNode](a,b)
+#template repeat(r,min): expr = repeatMatcher[TNode](r, min) 
+#proc repeat (a,b): expr = repeatMatcher[TNode](a,b)
+#proc repeat (a,b,c): Rule[TNode] = repeatMatcher[TNode](a,b,c)
+template grammar* (TNode: expr; body: stmt): stmt {.immediate.} =
+  proc chr (chars: varargs[char]): Rule[TNode] = charMatcher[TNode](chars)
+  proc chr (chars: set[char]): Rule[TNode]  = charMatcher[TNode](chars)
+  proc str (str: string): Rule[TNode] = strMatcher[TNode](str)
+  
+  proc stri (s: string): Rule[TNode] =
+    # case insensitive str
+    # probably more efficient to use a regex rule here
+    template accum (x): stmt =
+      if result.isNil:
+        result = x
+      else:
+        result = result and x
+    
+    for character in s.items:
+      if character in strutils.letters:
+        accum charMatcher[TNode](character.toLower, character.toUpper)
+      else:
+        accum charMatcher[TNode](character)
+  proc keyword (s: string): Rule[TNode] = 
+    str(s) and charMatcher[TNode](identChars).absent
+  
+  genGrammar(TNode, body)
+
+
+
+
+
+
+when isMainModule:
+
+  template echoCode (expression):stmt =
+    echo astToStr(Expression), ": ", expression
+    
+  when true:
+    block:
+      grammar(int):
+        space := chr(' ','\t','\L')
+        digit := chr({'0'..'9'})
+        digits := +digit
+        number := digits.save do (str: string) -> int: parseInt(str)
+        numbers := number and *(space and number)
+
+      echoCode digits.match("12311234")
+      echoCode number.match("9001")
+      echoCode numbers.match("99 44 11 6")
+      
+  when false:
+    let 
+      char_a = chr('a')
+      char_b = chr('b')
+      
+      ab = char_a and char_b
+      
+      a_pres_b = char_a and present(char_b) and (char_a or char_b)
+      
+
+    echoCode char_a.match("a")
+    echoCode ab.match("ab")
+    echoCode a_pres_b.match("ab")
+
+    echoCode chr('a').absent.match("b")
